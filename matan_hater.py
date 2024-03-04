050cbb70bb6466243f2783779184056b6e0e38b1
Yalta2012 <146913508+Yalta2012@users.noreply.github.com>
2024-03-04 21:40:44 +0500

    Add files via upload

--- matan_hater.py
+++ matan_hater.py
@@ -9,6 +9,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from progress.bar import IncrementalBar
 
+
+
 options = webdriver.EdgeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_experimental_option(
@@ -26,10 +28,11 @@ driver = webdriver.Edge(options=options)
 
 
 def aut(login, password):
+    t=time.time()
     driver.get("https://moodle.surgu.ru/login/index.php")
     driver.find_element(By.ID,"password").send_keys(password)
     driver.find_element(By.ID,"username").send_keys(login)
-    #print("AUT: SUCCSESS")
+    print(f"AUTHORIZATION COMPLITED IN {round(time.time()-t, 2)} SECONDS")
 
 def cont():
     time.sleep(1)
@@ -39,6 +42,7 @@ def ans(path):
     f=open(path,encoding="utf-8")
     start=f.readline().rstrip('\n')
     driver.get(f.readline().rstrip('\n'))
+    b=[]
     while True:
         line = f.readline().rstrip('\n')
         if not line:
@@ -55,24 +59,28 @@ def ans(path):
             for i in range(int(a[1])):
                 b=f.readline().split()
                 Select(driver.find_element(By.ID, b[0])).select_by_value(' '.join(b[1:]))
-            driver.find_element(By.ID,'id_submitbutton').click()          
+            driver.find_element(By.ID,'id_submitbutton').click()
+        elif a[0]=='input':
+            driver.find_element(By.ID,'id_answer').send_keys(a[1])
+            driver.find_element(By.ID,'id_submitbutton').click()
     driver.get(start)
 
 def main(path, times):
-    bar = IncrementalBar('Progress', max = times)
+    bar = IncrementalBar('PROGRESS', max = times)
     try:
         t=time.time()
         f=open("login.txt",encoding="utf-8")
         login=f.readline()
         password=f.readline()
         aut(login,password)
+        bar.start()
         for i in range(times):
             ans(path)
             bar.next()
         bar.finish()
-        print(i+1,' iterations complited in ', time.time()-t, 'seconds')
+        print(f"{i+1} ITERATIONS COMPLITED IN {round(time.time()-t, 2)} SECONDS")
     except Exception as e:
-        print("\nERROR")
+        print("\nERROR\n")
         print(e)
 
-main("4.4 config.txt", 10)
+main("4.5 config.txt", 196)
